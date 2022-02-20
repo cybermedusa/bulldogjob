464461f78d0b00533d036cfcdd86f5e8be11ca96
zuzka98 <zuzannakilar@gmail.com>
2022-02-20 20:33:41 +0100

    BULLDOGJOB added web scraping method

---
+++ jobs mid.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+
+with open('jobs_data_mid.csv', 'w', encoding='utf-8', newline='') as f:
+    thewriter = writer(f)
+    header = ['Title', 'Company', 'Location', 'Salary', 'Skills', 'Experience level']
+    thewriter.writerow(header)
+
+    pages = range(19)
+
+    for page in pages:
+        url = 'https://bulldogjob.pl/companies/jobs/s/experience_level,medium?page=' + str(page)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        jobs_list = soup.find_all('a', class_='search-list-item')
+        for j in jobs_list:
+            experience_lvl = 'Mid'
+            titles = j.find('div', class_='title').text.replace('\n', '')
+            companies = j.find('div', class_='company').text.replace('\n', '')
+            locations = j.find('div', class_='location').text.replace('\n', '')
+            if not j.find('ul', class_='tags'):
+                skill1 = ''
+            else:
+                skill1 = j.find('ul', class_='tags').text.replace('\n', ' ')
+
+            if not j.find('h4', class_='addon-stand-out'):
+                skill2 = ''
+            else:
+                skill2 = j.find('h4', class_='addon-stand-out').text.replace('\n', ' ')
+            if not j.find('div', class_='salary'):
+                salary = ''
+            else:
+                salary = j.find('div', class_='salary').text.replace('\n', ' ')
+            info = [titles, companies, locations, salary, skill1 + skill2, experience_lvl]
+            thewriter.writerow(info)
